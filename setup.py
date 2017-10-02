618ce8c42a11684ef9a781177186171eac94c295
Kevin Su <kevinsu1212@gmail.com>
2017-10-02 07:59:21 -0700

    implement helper functions

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-import numpy as numpy
-import matplotlib.pyplot as plt
-import h5py
-import scipy
-from PIL import Image
-from scipy import ndimage
-from lr_utils import load_dataset
+import numpy as np
+# import matplotlib.pyplot as plt
+# import h5py
+# import scipy
+# from PIL import Image
+# from scipy import ndimage
+# from lr_utils import load_dataset
 
-%matplotlib inline
+#matplotlib inline
+
+def sigmoid(z):
+    s = 1/(1+np.exp(-z))
+    return s
+
+print("sigmoid: " + str(sigmoid(np.array([0,2]))))
